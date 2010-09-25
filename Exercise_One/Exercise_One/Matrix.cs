3a862ed7afd0f8e5d78abca9341d2e26c61a5f65
Florian Bergmann <Bergmann.F@gmail.com>
2010-09-25 17:55:33 +0100

    Finished Exercise Ine

---
+++ Exercise_One/Exercise_One/Matrix.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Exercise_One
+{
+    class Matrix
+    {
+
+        public int[,] MultiplyMatrices(int[,] matrixOne, int[,] matrixTwo)
+        {
+            int rowsMatrixOne = matrixOne.GetLength((int) MatrixDimensions.DIMENSION_TWO);
+            int columnsMatrixTwo = matrixTwo.GetLength((int) MatrixDimensions.DIMENSION_ONE);
+            int rowsMatrixTwo = matrixTwo.GetLength((int)MatrixDimensions.DIMENSION_TWO);
+            int[,] resultMatrix = new int[rowsMatrixTwo, rowsMatrixOne];
+            
+            if (rowsMatrixOne != columnsMatrixTwo)
+            {
+                throw new ArgumentException("Provided matrices don't match: number of columns of matrix one need to match rows of matrix two.");
+            }
+            int cellValue = 0;
+
+            for (int i = 0; i < rowsMatrixOne; i++)
+            {
+                for (int k = 0; k < rowsMatrixTwo; k++)
+                {
+                    for (int j = 0; j < columnsMatrixTwo; j++)
+                    {
+                        cellValue += matrixOne[i, j] * matrixTwo[j, k];
+                    }
+                    resultMatrix[k, i] = cellValue;
+                    cellValue = 0;
+                } 
+            }
+            return resultMatrix;
+        }
+
+        public String PrintMatrix(int[,] matrix)
+        {
+            int lineLength = matrix.GetLength(0);
+            int j = 0;
+            String returnString = "\n| ";
+            foreach (int item in matrix)
+            {
+                returnString += item;
+                returnString += " | ";
+                j++;
+                if (j == lineLength)
+                {
+                    returnString += "\n";
+                    for (int i = 0; i < lineLength * 5; i++)
+                    {
+                        returnString += "-";
+                    }
+                    returnString += "\n";
+                    returnString += "| ";
+                    j = 0;
+                }
+            }
+            return returnString;
+        }
+    }
+
+    public enum MatrixDimensions
+    {
+        DIMENSION_ONE,
+        DIMENSION_TWO
+    }
+}
---
+++ Exercise_One/Exercise_One/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Exercise_One
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Matrix m = new Matrix();
+            int[,] matrixOne = new int[3, 3] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+            int[,] matrixTwo = new int[3, 2] { { 1, 2 }, { 3, 4 }, { 5, 6 } };
+            int[,] result = m.MultiplyMatrices(matrixOne, matrixTwo);
+            Console.WriteLine("Result is: {0}", m.PrintMatrix(result));
+            Console.ReadLine();
+        }
+    }
+}
